9d3162e044cd4c5b829c108b102d32844d56323e
Husaam Khan <husaamkhan04@gmail.com>
2024-06-06 09:56:39 -0400

    feat:

---
+++ script.py
@@ -0,0 +1,44 @@
+import os
+
+def createDir(path):
+    try:
+        os.mkdir(path)
+    except FileExistsError:
+        pass
+
+def createFolders():
+    try:
+        os.chdir("C:\\Users\\husaa\\Desktop")
+    except FileNotFoundError as e:
+        print(e)
+
+    createDir("Programs")
+
+    try:
+        os.chdir("C:\\Users\\husaa\\Documents")
+    except FileNotFoundError as e:
+        print(e)
+
+    createDir("Text Files")
+    createDir("PDF Documents")
+    createDir("Spreadsheets")
+    createDir("Word Documents")
+
+def checkDesktop():
+    os.chdir("C:\\Users\\husaa\\Desktop")
+    files = os.listdir()
+
+    for filename in files:
+        filename = filename.split('.')
+        if ( len(filename) == 1 ): # This means its a folder, not a files
+            continue
+
+        else:
+            if ( filename[1] == "txt" ):
+                pass
+
+def main():
+    createFolders()
+    checkDesktop()
+
+main()
